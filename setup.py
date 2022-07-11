b0164fe54082d609fdc5a76308934e1ab7290d6c
eveschoen <107372989+eveschoen@users.noreply.github.com>
2022-07-11 13:54:15 -0400

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'M14_HW',
+    version = '1.0.0',
+    url = 'https://github.com/eveschoen/M14_HW.git',
+    author = 'Eve Schoenrock',
+    author_email = 'ufu2rg@virginia.edu',
+    license = 'MIT',
+    description = 'This package details a booklovers journey through books.',
+    packages = ['M14_HW']
+)
